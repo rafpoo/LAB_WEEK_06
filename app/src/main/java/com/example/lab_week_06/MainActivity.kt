5583985e9b6957fe14ff83d452b9a7f498b74ffd
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-10-05 13:55:59 +0700

    feat: challenge lab kelar

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab_week_06
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,6 +27,80 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+
+    private val initialCats = listOf(CatModel(
+        Gender.Male,
+        CatBreed.BalineseJavanese,
+        "Fred",
+        "Silent and Deadly",
+        "https://cdn2.thecatapi.com/images/7dj.jpg"
+    ),
+        CatModel(
+            Gender.Female,
+            CatBreed.ExoticShortHair,
+            "Wilma",
+            "Cuddly Assassin",
+            "https://cdn2.thecatapi.com/images/egv.jpg"
+        ),
+        CatModel(
+            Gender.Unknown,
+            CatBreed.AmericanCurl,
+            "Curious George",
+            "Award Winning Investigator",
+            "https://cdn2.thecatapi.com/images/bar.jpg"
+        ))
+    private val classicCats = listOf(
+        CatModel(Gender.Male, CatBreed.BalineseJavanese, "Fred", "Silent and Deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
+        CatModel(Gender.Female, CatBreed.ExoticShortHair, "Wilma", "Cuddly Assassin", "https://cdn2.thecatapi.com/images/egv.jpg"),
+        CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Curious George", "Award Winning Investigator", "https://cdn2.thecatapi.com/images/bar.jpg")
+    )
+
+    private val elegantCats = listOf(
+        CatModel(Gender.Female, CatBreed.OrientalShorthair, "Cleopatra", "Queen of the couch", "https://cdn2.thecatapi.com/images/5sU22_O-2.jpg"),
+        CatModel(Gender.Male, CatBreed.Siamese, "Ramses", "Sunbathing expert", "https://cdn2.thecatapi.com/images/ai6JpsStP.jpg")
+    )
+
+    private val fluffyCats = listOf(
+        CatModel(Gender.Male, CatBreed.Persian, "Sir Fluffington", "Requires constant adoration", "https://cdn2.thecatapi.com/images/8g.jpg"),
+        CatModel(Gender.Female, CatBreed.Himalayan, "Snowbelle", "Master of the silent treatment", "https://cdn2.thecatapi.com/images/I_3-4tA60.jpg"),
+        CatModel(Gender.Male, CatBreed.Siberian, "Boris", "Loves poetry and long naps", "https://cdn2.thecatapi.com/images/3r3.jpg")
+    )
+
+    private val uniqueLookingCats = listOf(
+        CatModel(Gender.Female, CatBreed.Sphynx, "Noodles", "Wrinkly and wonderful", "https://cdn2.thecatapi.com/images/7M2m2P4iF.jpg"),
+        CatModel(Gender.Male, CatBreed.ScottishFold, "Barnaby", "Ears folded for aerodynamics", "https://cdn2.thecatapi.com/images/y4.jpg")
+    )
+
+    private val wildLookingCats = listOf(
+        CatModel(Gender.Male, CatBreed.Bengal, "Hunter", "Leopard of the living room", "https://cdn2.thecatapi.com/images/i2V2a-22-.jpg"),
+        CatModel(Gender.Female, CatBreed.Abyssinian, "Zola", "Graceful and energetic", "https://cdn2.thecatapi.com/images/yFhASx2b1.jpg"),
+        CatModel(Gender.Male, CatBreed.Savannah, "Simba", "King of all he surveys", "https://cdn2.thecatapi.com/images/5g6.jpg")
+    )
+
+    private val gentleGiants = listOf(
+        CatModel(Gender.Male, CatBreed.MaineCoon, "Moose", "Big, fluffy, and friendly", "https://cdn2.thecatapi.com/images/3j4.jpg"),
+        CatModel(Gender.Female, CatBreed.Ragdoll, "Princess Floof", "Goes limp when held", "https://cdn2.thecatapi.com/images/l3.jpg")
+    )
+
+    private val sleekCats = listOf(
+        CatModel(Gender.Female, CatBreed.RussianBlue, "Misty", "Shy but sweet", "https://cdn2.thecatapi.com/images/a5.jpg"),
+        CatModel(Gender.Male, CatBreed.Bombay, "Panther", "A shadow with eyes", "https://cdn2.thecatapi.com/images/i3n1M3TS2.jpg")
+    )
+
+    private val playfulCats = listOf(
+        CatModel(Gender.Male, CatBreed.JapaneseBobtail, "Bouncer", "Loves chasing laser dots", "https://cdn2.thecatapi.com/images/9o4.jpg"),
+        CatModel(Gender.Female, CatBreed.CornishRex, "Rexie", "Curious and adventurous", "https://cdn2.thecatapi.com/images/5p8.jpg")
+    )
+
+    private val mysteryCats = listOf(
+        CatModel(Gender.Unknown, CatBreed.Singapura, "Pip", "Small cat, big personality", "https://cdn2.thecatapi.com/images/k7g.jpg"),
+        CatModel(Gender.Unknown, CatBreed.TurkishAngora, "Ghost", "Appears only for food", "https://cdn2.thecatapi.com/images/a5s.jpg")
+    )
+
+    private val allLists = listOf(initialCats, classicCats, elegantCats, fluffyCats, uniqueLookingCats, wildLookingCats, gentleGiants, sleekCats, playfulCats, mysteryCats, elegantCats)
+
+    private var currentListIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -43,28 +118,18 @@ class MainActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
         catAdapter.setData(
-            listOf(CatModel(
-                Gender.Male,
-                CatBreed.BalineseJavanese,
-                "Fred",
-                "Silent and Deadly",
-                "https://cdn2.thecatapi.com/images/7dj.jpg"
-            ),
-                CatModel(
-                    Gender.Female,
-                    CatBreed.ExoticShortHair,
-                    "Wilma",
-                    "Cuddly Assassin",
-                    "https://cdn2.thecatapi.com/images/egv.jpg"
-                ),
-                CatModel(
-                    Gender.Unknown,
-                    CatBreed.AmericanCurl,
-                    "Curious George",
-                    "Award Winning Investigator",
-                    "https://cdn2.thecatapi.com/images/bar.jpg"
-                ))
+            allLists[currentListIndex]
         )
+
+        val changeListButton = findViewById<Button>(R.id.change_list_button)
+        changeListButton.setOnClickListener {
+            cycleThroughLists()
+        }
+    }
+
+    private fun cycleThroughLists() {
+        currentListIndex = (currentListIndex + 1) % allLists.size
+        catAdapter.setData(allLists[currentListIndex])
     }
 
     private fun showSelectionDialog(cat: CatModel) {
--- app/src/main/java/com/example/lab_week_06/model/CatBreed.kt
+++ app/src/main/java/com/example/lab_week_06/model/CatBreed.kt
@@ -1,5 +1,5 @@
 package com.example.lab_week_06.model
 
 enum class CatBreed {
-    AmericanCurl, BalineseJavanese, ExoticShortHair
+    AmericanCurl, BalineseJavanese, ExoticShortHair, OrientalShorthair, Persian, Siamese, Himalayan, Sphynx, RussianBlue, Bombay, MaineCoon, Ragdoll, Bengali, CornishRex, Savannah, TurkishAngora, JapaneseBobtail, Singapura, Siberian, Abyssinian, Bengal, ScottishFold
 }
